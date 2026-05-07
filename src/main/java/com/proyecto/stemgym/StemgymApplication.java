42a0004dad6c5f8f94926efd5036a1d4192353b1
martinllanelijorge <martinllaneli001.alu@stemgranada.com>
2026-05-07 17:19:42 +0200

    feat: init proyecto stemgym con maven y dependencias JPA, SPRING WEB y H2

---
+++ src/main/java/com/proyecto/stemgym/StemgymApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.stemgym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StemgymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StemgymApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/stemgym/StemgymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.stemgym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StemgymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
