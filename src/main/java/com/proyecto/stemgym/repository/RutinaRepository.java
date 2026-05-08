08cea758c74cbe87ad561c0b894564f3383bb970
martinllanelijorge <martinllanelijorge001.alu@stemgranada.com>
2026-05-08 15:40:30 +0200

    feat: Crear RutinaRepository para la entidad Rutina

---
+++ src/main/java/com/proyecto/stemgym/repository/RutinaRepository.java
@@ -0,0 +1,9 @@
+package com.proyecto.stemgym.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.proyecto.stemgym.entity.Rutina;
+
+public interface RutinaRepository extends JpaRepository<Rutina, Long> {
+
+}
