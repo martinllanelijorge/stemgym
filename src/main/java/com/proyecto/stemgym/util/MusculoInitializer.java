6c8bcdc7aed29771bf711d371d8ed8be62a57fda
martinllanelijorge <martinllanelijorge001.alu@stemgranada.com>
2026-05-08 19:38:01 +0200

    feat: Crear MusculoInitializar para que la api comience con unos músculos precreados

---
+++ src/main/java/com/proyecto/stemgym/util/MusculoInitializer.java
@@ -0,0 +1,52 @@
+package com.proyecto.stemgym.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.proyecto.stemgym.controller.MusculoController;
+import com.proyecto.stemgym.entity.Musculo;
+
+import jakarta.transaction.Transactional;
+
+@Component
+public class MusculoInitializer {
+
+    @Autowired
+    private MusculoController musculoController;
+
+    @Transactional
+    public void inicializarMusculos() {
+
+        // Creación de los músculos iniciales de la DB
+        Musculo pectorales = new Musculo("pectorales", 2, 12, 20,
+                "https://m.media-amazon.com/images/I/61WNnhYl2CL._AC_UF894,1000_QL80_.jpg");
+        Musculo dorsales = new Musculo("dorsales", 2, 14, 22,
+                "https://www.labolsadelcorredor.com/wp-content/uploads/2020/03/dorsales-e1584814066729.jpg");
+        Musculo trapecio = new Musculo("trapecio", 2, 14, 22,
+                "https://www.ericfavre.com/lifestyle/es/wp-content/uploads/sites/8/2021/09/trapeze.jpg");
+        Musculo deltoides = new Musculo("deltoides", 2, 12, 20,
+                "https://previews.123rf.com/images/woodoo007/woodoo0071510/woodoo007151000245/47041842-deltoid-muscles-anatomy-map.jpg");
+        Musculo triceps = new Musculo("tríceps", 2, 10, 14,
+                "https://www.cambiatufisico.com/wp-content/uploads/anatomia-triceps.jpg");
+        Musculo biceps = new Musculo("bíceps", 2, 14, 20,
+                "https://www.rehabmypatient.com/media/uploads/articles/Biceps_Brachii_Tendinopathy.jpg");
+        Musculo cuadriceps = new Musculo("cuádriceps", 2, 12, 18,
+                "https://www.escuelaculturismonatural.com/wp-content/uploads/elementor/thumbs/Quadriceps-muscles-e1660481667161-pt94479g84lgtxwid7byvpxbpswzq758ltel76ee9k.png");
+        Musculo isquios = new Musculo("isquios", 2, 12, 16,
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTa2WcLNTEgJpnOPOzAl2U474BrRIpEm1AkJA&s");
+        Musculo gluteos = new Musculo("gluteos", 2, 12, 16,
+                "https://somasalud.es/wp-content/uploads/2019/03/musculo-gluteo.jpg");
+
+        // Guardado de los musculos en la DB
+        musculoController.crearNuevoMusculo(pectorales);
+        musculoController.crearNuevoMusculo(dorsales);
+        musculoController.crearNuevoMusculo(trapecio);
+        musculoController.crearNuevoMusculo(deltoides);
+        musculoController.crearNuevoMusculo(triceps);
+        musculoController.crearNuevoMusculo(biceps);
+        musculoController.crearNuevoMusculo(cuadriceps);
+        musculoController.crearNuevoMusculo(isquios);
+        musculoController.crearNuevoMusculo(gluteos);
+    }
+
+}
