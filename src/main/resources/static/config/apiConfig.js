ddde2724f9dfb587efe35ddff6850238ac64e2ce
martinllanelijorge <martinllanelijorge001.alu@stemgranada.com>
2026-05-10 18:45:50 +0200

    feat: Agregar apiConfig.js para tener los endpoints de la api

---
+++ src/main/resources/static/config/apiConfig.js
@@ -0,0 +1,7 @@
+export const API_CONFIG = {
+    apiUrl: 'http://localhost:8080/stemgymapi',
+    apiUrlClientes: `${apiUrl}/clientes`,
+    apiUrlMusculos: `${apiUrl}/musculos`,
+    apiUrlEjercicios: `${apiUrl}/ejercicios`,
+    apiUrlRutinas: `${apiUrl}/rutinas`
+};
