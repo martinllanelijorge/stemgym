e01ce9bd9a860c7e762772abcf0223b516b8aa51
martinllanelijorge <martinllanelijorge001.alu@stemgranada.com>
2026-05-10 19:15:00 +0200

    feat: Crear petición get en clientes.js para la lista de clientes

---
+++ src/main/resources/static/pages/clientes.js
@@ -0,0 +1,19 @@
+import { hacerFetch } from '../utils/apiUtils.js';
+
+// Lugar donde se va a almacenar la lista
+const zonaListaClientes = document.getElementById("listaClientes")
+// Lista de clientes de la api
+const clientes = await hacerFetch("/clientes")
+
+// Construcción de la lista en el html
+zonaListaClientes.innerHTML = ""
+
+if (clientes.length === 0) {
+    zonaListaClientes.innerHTML = '<li class="mensaje-error-api">No se han encontrado clientes</li>'
+} else {
+    for (let cliente of clientes) {
+        zonaListaClientes.innerHTML += `<li>${cliente.nombre}</li>`
+    }
+}
+
+
