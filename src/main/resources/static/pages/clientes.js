90eff46e1eb3a73aeaa3eba4b7d4cfa3abc6624f
martinllanelijorge <martinllanelijorge001.alu@stemgranada.com>
2026-05-10 20:41:06 +0200

    feat: Agregar funcionalidad al buscador por nombre para mostrar solo los comiencen igual

--- src/main/resources/static/pages/clientes.js
+++ src/main/resources/static/pages/clientes.js
@@ -4,10 +4,31 @@ import { hacerFetch } from '../utils/apiUtils.js';
 const zonaListaClientes = document.getElementById("listaClientes")
 // Lista de clientes de la api
 const clientes = await hacerFetch("GET", "/clientes")
+// Buscador de clientes
+const buscadorCliente = document.getElementById("buscadorCliente")
 
 // Construcción de la lista en el html
 zonaListaClientes.innerHTML = ""
 
+
+// Función para compara el cliente con lo buscado en el buscador
+function mostrarClienteBuscador(textoBusqueda) {
+    for (let fila of document.querySelectorAll("#listaClientes li")) {
+        // Se extrae el texto de cada fila y se pone en minúscula para comparar
+        let nombre = fila.querySelector("span")
+        nombre = nombre.textContent.toLowerCase()
+
+        // EL NOMBRE COMIENZA POR LO ESCRITO EN EL BUSCADOR - SE MUESTRA
+        if (nombre.startsWith(textoBusqueda)) {
+            fila.style.display = ""
+        // EL NOMBRE NO COMIENZA POR LO ESCRITO - NO SE MUESTRA
+        } else {
+            fila.style.display = "none"
+        }
+    }
+}
+
+// Añade los clientes en lista al cargar la página
 if (clientes.length === 0) {
     zonaListaClientes.innerHTML = '<li class="mensaje-error-api">No se han encontrado clientes</li>'
 } else {
@@ -21,4 +42,9 @@ if (clientes.length === 0) {
     }
 }
 
+// Acción del buscador por nombre
+buscadorCliente.addEventListener("input", function () {
+    const busquedaActual = this.value.toLowerCase();
+    mostrarClienteBuscador(busquedaActual)
+});
 
