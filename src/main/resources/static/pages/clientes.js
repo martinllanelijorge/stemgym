e0ac8fc37fd77d0be4a5b97a97ab4edd5adb7fca
martinllanelijorge <martinllanelijorge001.alu@stemgranada.com>
2026-05-10 19:28:46 +0200

    fix: Arreglar bug en GET, falta de parametro para clientes.js

--- src/main/resources/static/pages/clientes.js
+++ src/main/resources/static/pages/clientes.js
@@ -3,7 +3,7 @@ import { hacerFetch } from '../utils/apiUtils.js';
 // Lugar donde se va a almacenar la lista
 const zonaListaClientes = document.getElementById("listaClientes")
 // Lista de clientes de la api
-const clientes = await hacerFetch("/clientes")
+const clientes = await hacerFetch("GET", "/clientes")
 
 // Construcción de la lista en el html
 zonaListaClientes.innerHTML = ""
