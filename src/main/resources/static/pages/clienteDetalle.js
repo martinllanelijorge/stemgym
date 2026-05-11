ba1a32088cc89cbb8c1834e30e5e7ea3352da42c
martinllanelijorge <martinllanelijorge001.alu@stemgranada.com>
2026-05-11 19:37:25 +0200

    style: Decorar tarjeta detalle de cliente en css

--- src/main/resources/static/pages/clienteDetalle.js
+++ src/main/resources/static/pages/clienteDetalle.js
@@ -3,6 +3,7 @@ import { hacerFetch } from '../utils/apiUtils.js'
 // Zona donde se pondrán los datos del cliente
 const zonaDatosCliente = document.getElementById("zonaCliente")
 const zonaListaRutinas = document.getElementById("listaRutinas")
+const zonaNuevaRutina = document.getElementById("zonaNuevaRutina")
 
 // Obtención de la id del cliente
 const parametros = new URLSearchParams(window.location.search)
@@ -10,7 +11,7 @@ const idCliente = parametros.get('id')
 
 // Uso de fetch para obtención de los datos del cliente
 const cliente = await hacerFetch(`GET`, `/clientes/${idCliente}`)
-const listaRutinas = await hacerFetch(`GET`, `/clientes/${idCliente}`)
+const listaRutinas = await hacerFetch(`GET`, `/clientes/${idCliente}/rutinas`)
 
 // Función para decorar en forma de emoticono si se cumplió el objetivo
 function visualizarObjetivoCumplido(objetivoCumplido){
@@ -30,11 +31,18 @@ zonaDatosCliente.innerHTML = `
         <p><strong>Género: </strong>${cliente.genero}</p>
         <p><strong>Peso actual: </strong>${cliente.pesoActual} kg</p>
         <p><strong>Objetivo: </strong>${cliente.pesoObjetivo} kg</p>
-        <p><strong>Objetivo alcanzado: </strong>${visualizarObjetivoCumplido(cliente.pesoObjetivo)}</p>
+        <p><strong>Objetivo alcanzado: </strong>${visualizarObjetivoCumplido(cliente.objetivoCumplido)}</p>
     </div>`
 
-// Rutina del cliente
+// Acceso a creación de una nueva rutina
+zonaNuevaRutina.innerHTML = `<a class="aniadir-nuevo" href="nuevaRutina.html?id=${cliente.id}">Crear nueva rutina</a>`
+
+// Rutina del cliente - filas - con cabecera
 for (let rutina of listaRutinas) {
-    zonaListaRutinas.innerHTML += `<li>${rutina.nombre}</li>`
+    zonaListaRutinas.innerHTML += `
+    <li>
+        <p>${rutina.nombre}</p>
+        <a href="rutinaDetalle.html?id=${rutina.id}" class="btn-ver-mas">Ver más</a>
+    </li>`
 }
 
