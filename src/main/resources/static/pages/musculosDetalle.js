ae430936368dcfdbb35ebdac74ee0b6b083b9e91
martinllanelijorge <martinllanelijorge001.alu@stemgranada.com>
2026-05-12 20:56:34 +0200

    wip: Desarrollar musculoDetalle

--- src/main/resources/static/pages/musculosDetalle.js
+++ src/main/resources/static/pages/musculosDetalle.js
@@ -11,3 +11,25 @@ const idMusculo = parametros.get('id')
 // Uso de fetch para obtención de los datos del músculo
 const musculo = await hacerFetch(`GET`, `/musculos/${idMusculo}`)
 const ejercicios = await hacerFetch(`GET`, `/musculos/${idMusculo}/ejercicios`)
+
+// ============== MAIN ============= //
+// Zona de datos del músculo 
+zonaMusculo.innerHTML = `
+    <h2>${musculo.nombre}</h2>
+    <div>
+        <img src=${musculo.urlImagen}/>
+        <table>
+            <thead>
+                <tr>
+                    <th>Frecuencia Recomendada</th>
+                    <th>MIN - MAX Volumen Semanal</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>${musculo.frecuenciaRecomendada}</td>
+                    <td>${musculo.minVolumenSemanal} - ${maxVolumenSemanal}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>`
